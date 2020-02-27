878aef19e5ce6062dde9ecf764db69a2d86d4c2d
meetchen <17408070327@stu.xust.edu.cn>
2020-02-27 14:24:55 +0800

    commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import video
+import time
+import sys
+import numpy as np
+import cv2
+import time
+
+video = video.Video()
+time.sleep(1.0)   #let camera autofocus + autosaturation settle
+video.nextFrame()
+video.testBackgroundFrame()
+
+while 1:
+        #get next frame of video
+        video.nextFrame()
+        video.testBackgroundFrame() #press n to delete background?
+        video.updateBackground()
+        video.compare()
+        video.showFrame()
+        video.testSettings()
+        k = cv2.waitKey(30) & 0xff
+        if video.testDestroy():
+        # if k == 27:
+                break
+
+#video.release()
+cv2.destroyAllWindows()
+sys.exit()
