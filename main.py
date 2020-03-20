8d2da4fa67d8ca64c7fc3d3fc220c9882b09c4dd
meetchen <17408070327@stu.xust.edu.cn>
2020-03-20 10:03:06 +0800

    use python 3.5.3

--- main.py
+++ main.py
@@ -6,26 +6,23 @@ import cv2
 import time
 
 video = video.Video()
-time.sleep(1.0)  # let camera autofocus + autosaturation settle
+time.sleep(1.0)   #let camera autofocus + autosaturation settle
 video.nextFrame()
 video.testBackgroundFrame()
 
 while 1:
-    try:
-        # get next frame of video
+        #get next frame of video
         video.nextFrame()
         video.testBackgroundFrame()  # press n to delete background?
         video.updateBackground()
         video.compare()
-        video.showFrame()
+        # video.showFrame()
         video.testSettings()
         k = cv2.waitKey(30) & 0xff
         if video.testDestroy():
-            # if k == 27:
-            break
-    except:
-        pass
+                # if k == 27:
+                break
 
-# video.release()
+#video.release()
 cv2.destroyAllWindows()
 sys.exit()
